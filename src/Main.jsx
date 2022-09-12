8770043a39f53478b5a2e00ef7a0bddc808471d0
서민교 <67179490+hwaun123@users.noreply.github.com>
2022-09-13 01:42:18 +0900

    FORM

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,13 @@
 const Main = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
     <>
-      <div>메인임</div>
+      <form onSubmit={handleSubmit}>
+        <input type="text"></input>
+        <button type="submit">검색</button>
+      </form>
     </>
   );
 };
--- src/ViewStock.jsx
+++ src/ViewStock.jsx
@@ -1,5 +1,9 @@
 const ViewStock = () => {
-  return <></>;
+  return (
+    <>
+      <div>주식 상세보기 페이지입니다</div>
+    </>
+  );
 };
 
 export default ViewStock;
