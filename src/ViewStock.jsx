09f110a55e5b4e4af71b2488c3138db72918ccaa
서민교 <67179490+hwaun123@users.noreply.github.com>
2022-09-14 21:53:33 +0900

    DATA REFINE

--- src/ViewStock.jsx
+++ src/ViewStock.jsx
@@ -41,12 +41,28 @@ const ViewStock = () => {
     return <div>데이터를 가져올 수 없습니다</div>;
   }
 
+  console.log(stockData);
   return (
     <>
       {stockData.map((item) => (
-        <li>
-          {item.itmsNm} : {item.basDt}{" "}
-        </li>
+        <>
+          <li>{item.itmsNm}</li>
+          <li>종목 코드 : {item.isinCd}</li>
+          <li>{item.basDt}</li>
+          <li>시장: {item.mrktCtg}</li>
+          <li>
+            {item.basDt}일자 시작가 {item.mkp}
+          </li>
+          <li>
+            {item.basDt}일자 최고가 {item.hipr}
+          </li>
+          <li>
+            {item.basDt}일자 최저가 {item.lopr}
+          </li>
+          <li>
+            전일 대비 등락 : {item.vs} 전일 대비 등락비 : {item.fltRt}
+          </li>
+        </>
       ))}
     </>
   );
